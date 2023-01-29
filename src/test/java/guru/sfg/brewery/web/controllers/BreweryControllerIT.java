b18ce8b0a53cb39fdbbf0152d14b4b936e087043
bartek <smalecbartlomiej@gmail.com>
2023-01-29 14:53:49 +0100

    Update security to access endpoitn with specific role

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BreweryControllerIT.java
@@ -0,0 +1,69 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class BreweryControllerIT extends BaseIT {
+
+    public static final String URL_TEMPLATE = "/brewery/breweries";
+    public static final String REST_API_TEMPLATE = "/brewery/api/v1/breweries";
+
+    @Test
+    void listBreweriesCUSTOMER() throws Exception {
+        mockMvc.perform(get(URL_TEMPLATE)
+                        .with(httpBasic("scott", "tiger")))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void listBreweriesADMIN() throws Exception {
+        mockMvc.perform(get(URL_TEMPLATE)
+                        .with(httpBasic("spring", "guru")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void listBreweriesUSER() throws Exception {
+        mockMvc.perform(get(URL_TEMPLATE)
+                        .with(httpBasic("user", "password")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void listBreweriesNOAUTH() throws Exception {
+        mockMvc.perform(get(URL_TEMPLATE))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void getBreweriesJsonCUSTOMER() throws Exception {
+        mockMvc.perform(get(REST_API_TEMPLATE)
+                        .with(httpBasic("scott", "tiger")))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void getBreweriesJsonADMIN() throws Exception {
+        mockMvc.perform(get(REST_API_TEMPLATE)
+                        .with(httpBasic("spring", "guru")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void getBreweriesJsonUSER() throws Exception {
+        mockMvc.perform(get(REST_API_TEMPLATE)
+                        .with(httpBasic("user", "password")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void getBreweriesJsonNOAUTH() throws Exception {
+        mockMvc.perform(get(REST_API_TEMPLATE))
+                .andExpect(status().isUnauthorized());
+    }
+}
--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -1,10 +1,8 @@
 package guru.sfg.brewery.web.controllers.api;
 
 import guru.sfg.brewery.web.controllers.BaseIT;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
@@ -49,6 +47,20 @@ public class BeerRestControllerIT extends BaseIT {
                         .header("Api-Key", "spring")
                         .header("Api-Secret", "guru"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeerHttpBasicUserRole() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                        .with(httpBasic("user", "password")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void deleteBeerHttpBasicCustomerRole() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                        .with(httpBasic("scott", "tiger")))
+                .andExpect(status().isForbidden());
     }
 
     @Test
