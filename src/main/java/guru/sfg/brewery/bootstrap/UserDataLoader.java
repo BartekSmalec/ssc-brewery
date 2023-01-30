811a1207210cd0930a72638d126819ab7d643ec9
bartek <smalecbartlomiej@gmail.com>
2023-01-30 18:06:23 +0100

    Created custom annotation for beer permission

--- src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
+++ src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -3,7 +3,7 @@ package guru.sfg.brewery.bootstrap;
 import guru.sfg.brewery.domain.security.Authority;
 import guru.sfg.brewery.domain.security.Role;
 import guru.sfg.brewery.domain.security.User;
-import guru.sfg.brewery.repositories.RoleRepository;
+import guru.sfg.brewery.repositories.security.RoleRepository;
 import guru.sfg.brewery.repositories.security.AuthorityRepository;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
