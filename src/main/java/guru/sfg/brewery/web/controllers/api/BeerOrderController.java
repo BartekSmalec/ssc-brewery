e3cda98d834f288224ad821d6126243060541d30
bartek <smalecbartlomiej@gmail.com>
2023-02-01 10:16:52 +0100

    Fix problemw with mapstruct

--- src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
@@ -26,8 +26,7 @@ public class BeerOrderController {
         this.beerOrderService = beerOrderService;
     }
 
-    @PreAuthorize("hasAuthority('order.read') OR hasAuthority('customer.order.read') AND @beerOrderAuthenticationManager.customerIdMatches(authentication, #customerId)")// +
-            //" AND @beerOrderAuthenticationManger.customerIdMatches(authentication, #customerId )")
+    @PreAuthorize("hasAuthority('order.read') OR hasAuthority('customer.order.read') AND @beerOrderAuthenticationManager.customerIdMatches(authentication, #customerId)")
     @GetMapping("orders")
     public BeerOrderPagedList listOrders(@PathVariable("customerId") UUID customerId,
                                          @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
@@ -52,7 +51,7 @@ public class BeerOrderController {
 
     @PreAuthorize("hasAuthority('order.read') OR " +
             "hasAuthority('customer.order.read') " +
-            " AND @beerOrderAuthenticationManger.customerIdMatches(authentication, #customerId )")
+            " AND @beerOrderAuthenticationManager.customerIdMatches(authentication, #customerId )")
     @GetMapping("orders/{orderId}")
     public BeerOrderDto getOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
         return beerOrderService.getOrderById(customerId, orderId);
