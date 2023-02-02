b8ec444e3fb13b5f54c1b841481fca5db8d1d2e1
bartek <smalecbartlomiej@gmail.com>
2023-02-02 13:10:12 +0100

    Added saving to database login failures

--- src/main/java/guru/sfg/brewery/security/listners/AuthenticationFailureListener.java
+++ src/main/java/guru/sfg/brewery/security/listners/AuthenticationFailureListener.java
@@ -1,5 +1,9 @@
 package guru.sfg.brewery.security.listners;
 
+import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.repositories.security.LoginFailureRepository;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -8,23 +12,37 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class AuthenticationFailureListener {
+
+    private final LoginFailureRepository loginFailureRepository;
+    private final UserRepository userRepository;
 
     @EventListener
     public void listen(AuthenticationFailureBadCredentialsEvent event) {
         log.debug("Authentication failure");
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            LoginFailure.LoginFailureBuilder builder =  LoginFailure.builder();
+
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
             if (token.getPrincipal() instanceof String && token.getCredentials() instanceof String) {
-                log.debug("User name: {}  and credentials: {}", token.getPrincipal(), token.getCredentials());
+                String username = (String)token.getPrincipal();
+                builder.username(username);
+                if(userRepository.findByUsername(username).isPresent()){
+                    builder.user(userRepository.findByUsername(username).get());
+                }
 
+                log.debug("User name: {}  and credentials: {}", token.getPrincipal(), token.getCredentials());
             }
             if (token.getDetails() instanceof WebAuthenticationDetails) {
                 WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
-
+                builder.sourceIp(details.getRemoteAddress());
                 log.debug("Source IP: " + details.getRemoteAddress());
             }
+
+            LoginFailure loginFailure = loginFailureRepository.save(builder.build());
+            log.debug("Login failure id: {}", loginFailure.getId());
         }
     }
 }
